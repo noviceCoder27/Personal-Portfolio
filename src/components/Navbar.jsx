5c4f7e63e82be32cd52a555c8103e1280395fb68
mdGoswami <u19cse1001@cit.ac.in>
2023-10-06 22:11:35 +0530

    feat: add animation in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,20 @@ import { ThemeContext } from './../utils/Context';
 import {PiMoonLight} from 'react-icons/pi';
 import {RxCross1} from 'react-icons/rx';
 import { useState } from 'react';
+import {motion} from 'framer-motion';
+
 
 
 
 const Navbar = () => {
     const {isDark,setTheme} = useContext(ThemeContext);
     const [showMenu,setShowMenu] = useState(false);
+
+    const menuStyles = {
+        open: {opacity: 1, x: 0},
+        closed: {opacity: 1, x: "-100%"}
+    }
+
     return (
         <div>
             <div className='flex items-center justify-between px-4 py-6'>
@@ -30,17 +38,21 @@ const Navbar = () => {
                 <div className='flex items-center gap-4 '>
                     {isDark && <PiSunLight className={`text-2xl cursor-pointer ${isDark ? 'text-white': 'text=black'}`} onClick={setTheme}/>}
                     {!isDark && <PiMoonLight className={`text-2xl cursor-pointer ${isDark ? 'text-white': 'text=black'}`} onClick={setTheme}/>}
-                    {!showMenu && <AiOutlineMenu onClick = {() => setShowMenu(prev => !prev)} className={`text-4xl cursor-pointer ${isDark ? 'text-white': 'text=black'}`}/>}
+                    {!showMenu && <AiOutlineMenu onClick = {() => setShowMenu(prev => !prev)} className={`md:hidden text-4xl cursor-pointer ${isDark ? 'text-white': 'text-black'}`}/>}
                     {showMenu && <RxCross1 onClick = {() => setShowMenu(prev => !prev)} className={`text-4xl cursor-pointer ${isDark ? 'text-white': 'text=black'}`}/>}
                 </div>
             </div>
-            <div className={`min-md:hidden absolute backdrop-blur-sm transition ease-in-out px-4 flex flex-col gap-4 ${isDark ? 'text-white': 'text-black'} ${showMenu ? 'opacity-100': 'opacity-0'}`}>
+            <motion.div 
+            className={`absolute md:hidden min-w-[20vw] backdrop-blur-sm px-4 flex flex-col gap-4 ${isDark ? 'text-white': 'text-black'}`}
+            animate = {showMenu ? "open": "closed"}
+            variants={menuStyles}
+            >
                 <div>Home</div>
                 <div>About</div>
                 <div>Skills</div>
                 <div>Projects</div>
                 <div>Education</div>
-            </div>
+            </motion.div>
         </div>
     )
 }
